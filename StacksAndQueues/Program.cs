b44d2f7600750fd6148466dad73984ec8c233f95
Akash <akashmane12598@gmail.com>
2020-10-21 16:02:40 +0530

    [Akash] Add. Master Code

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stacks and Queues using Linked List!");
+        }
+    }
+}
