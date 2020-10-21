59b6b492030a5c2a2a87e74b591df829ae44acf6
Akash <akashmane12598@gmail.com>
2020-10-21 16:34:24 +0530

    [Akash] Add. Creating Stack and Pushing Elements UC1 code

---
+++ StacksAndQueues/LinkedListStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    public class LinkedListStack
+    {
+        internal Node head;
+        /// <summary>
+        /// UC 1
+        /// Logic is just add new element at the start of LinkedList and then print the List. It will work as Stack
+        /// </summary>
+        /// <param name="data"></param>
+        public void Push(int data)
+        {
+            Console.WriteLine("Pushing {0} into Stack",data);
+            Node node = new Node(data);
+
+            if (head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node top = this.head;   //head in LinkedList == Top in stack
+                node.next = top;
+                this.head = node;
+            }
+
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            Console.WriteLine("Displaying Elements of Stack:");
+
+            while (temp != null)
+            {
+                Console.Write(temp.data+" ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -7,6 +7,11 @@ namespace StacksAndQueues
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stacks and Queues using Linked List!");
+            LinkedListStack stack = new LinkedListStack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
         }
     }
 }
